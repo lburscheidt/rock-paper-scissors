bc1ebab1a4772c2fbf62379145b952a249581965
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-27 16:52:36 +0200

    Get randomised computer move, prompt for human move
    Prompt for human move normalises human input to lowercase string and keeps asking for input if input is invalid. Returns human move once one is input correctly.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 10);
+  if (random <= 3) {
+    return "rock";
+  } else if (random >= 4 && random <= 6) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("What's your move?");
+  if (
+    humanChoice.toLowerCase() != "rock" &&
+    humanChoice.toLowerCase() != "paper" &&
+    humanChoice.toLowerCase() != "scissors"
+  ) {humanChoice = getHumanChoice();
+   return humanChoice;
+    
+  } else {
+    return humanChoice.toLowerCase();
+  }
+}
+
+console.log(getHumanChoice());
