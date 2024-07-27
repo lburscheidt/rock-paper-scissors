f01e05c99353b8b02451c5ce4a75f87e999443cd
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-27 18:25:17 +0200

    Add logic to play five rounds

--- script.js
+++ script.js
@@ -23,28 +23,37 @@ function getHumanChoice() {
   }
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice == computerChoice) {
-    console.log(
-      `You played: ${humanChoice}. The computer played: ${computerChoice}. It's a draw! Play another round.`
-    );
-  } else if (
-    (humanChoice == "paper" && computerChoice == "rock") ||
-    (humanChoice == "rock" && computerChoice == "scissors") ||
-    (humanChoice == "scissors" && computerChoice == "paper")
-  ) {
-    humanScore += 1;
-    console.log(
-      `You played: ${humanChoice}. The computer played: ${computerChoice}.You win! ${humanChoice} beats ${computerChoice}. Your score is ${humanScore}. The computer score is ${computerScore}.`
-    );
-  } else {
-    computerScore += 1;
-    console.log(
-      `You played: ${humanChoice}. The computer played: ${computerChoice}. You lose! ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. The computer score is ${computerScore}.`
-    );
-  }
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+      console.log(
+        `You played: ${humanChoice}. The computer played: ${computerChoice}. It's a draw! Play another round.`
+      );
+    } else if (
+      (humanChoice == "paper" && computerChoice == "rock") ||
+      (humanChoice == "rock" && computerChoice == "scissors") ||
+      (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+      humanScore += 1;
+      console.log(
+        `You played: ${humanChoice}. The computer played: ${computerChoice}.You win! ${humanChoice} beats ${computerChoice}. Your score is ${humanScore}. The computer score is ${computerScore}.`
+      );
+    } else {
+      computerScore += 1;
+      console.log(
+        `You played: ${humanChoice}. The computer played: ${computerChoice}. You lose! ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. The computer score is ${computerScore}.`
+      );
+    }
+  }
+  for (let i = 1; i < 6; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  console.log("Five rounds are over");
+}
+
+playGame();
