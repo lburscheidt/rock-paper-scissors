2ac6fdf41a7f9b4fc8badc00934967ff335bf0de
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-27 17:14:55 +0200

    Add logic to play a single round

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 10);
   if (random <= 3) {
-    return "rock";
+    return "Rock";
   } else if (random >= 4 && random <= 6) {
-    return "paper";
+    return "Paper";
   } else {
-    return "scissors";
+    return "Scissors";
   }
 }
 
@@ -19,8 +22,30 @@ function getHumanChoice() {
    return humanChoice;
     
   } else {
-    return humanChoice.toLowerCase();
+    return humanChoice[0].toUpperCase() +humanChoice.slice(1).toLowerCase();
   }
 }
+/*Logic for a single round*/
 
-console.log(getHumanChoice());
+
+
+function playRound(humanChoice, computerChoice) {
+   let msg = "";
+    if( humanChoice == computerChoice){
+console.log(`You played: ${humanChoice}. The computer played: ${computerChoice}.It's a draw! Play another round.`)
+   }
+   else if((humanChoice == "paper" && computerChoice == "rock")||(humanChoice == "rock" && computerChoice == "scissors")||(humanChoice == "scissors" && computerChoice == "paper")){
+    humanScore += 1;
+    console.log(`You played: ${humanChoice}. The computer played: ${computerChoice}.You win! ${humanChoice} beats ${computerChoice}. Your score is ${humanScore}. The computer score is ${computerScore}.`);
+   
+   }
+else{
+    computerScore +=1;
+    console.log(`ou played: ${humanChoice}. The computer played: ${computerChoice}. You lose! ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. The computer score is ${computerScore}.`);
+}
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
