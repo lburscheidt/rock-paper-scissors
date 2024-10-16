23037339937909cb33e40a514692dc67c67623d5
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-16 23:13:22 +0200

    feat: add function to play a single round

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 function getHumanChoice() {
   let humanChoice = prompt("Please enter your move: rock, paper, or scissors");
@@ -29,4 +29,20 @@ function getHumanChoice() {
   }
 }
 
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "paper") ||
+    (humanChoice == "paper" && humanChoice == "rock")
+  ) {
+    ++humanScore;
+    return `You win! ${humanChoice} beats ${computerChoice}.`;
+  } else {
+    ++computerScore;
+    return `Computer wins! ${computerChoice} beats ${humanChoice}.`;
+  }
+}
+
+console.log(playRound(getComputerChoice(), getHumanChoice()));
