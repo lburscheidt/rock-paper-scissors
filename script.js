2255be675d033c30bad39ec2ae24b12dfacf07df
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-16 22:46:02 +0200

    feat: add function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+function getComputerChoice() {
+  let computerChoiceNumber = Math.floor(Math.random() * 3);
+  if (computerChoiceNumber === 0) {
+    return "rock";
+  } else if (computerChoiceNumber === 1) {
+    return "paper";
+  } else if (computerChoiceNumber === 2) {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
