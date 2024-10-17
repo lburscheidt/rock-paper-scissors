8f7527eb6ebb7d1d4b9642d9722237470085b91a
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-17 16:11:39 +0200

    feat: add event listeners to buttons

--- script.js
+++ script.js
@@ -9,36 +9,11 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt(
-    "Please enter your move: rock, paper, or scissors",
-  ).toLowerCase();
-  if (
-    humanChoice == "rock" ||
-    humanChoice == "paper" ||
-    humanChoice == "scissors"
-  ) {
-    return humanChoice;
-  } else {
-    return (humanChoice = prompt(
-      "Sorry, that's not a valid move. Please select from rock, paper, or scissors.",
-    ));
-  }
-}
-
-function capitalize(string) {
-  let firstLetter = string.slice(0, 1).toUpperCase();
-  let lowerCase = string.slice(1).toLowerCase();
-  let newString = firstLetter + lowerCase;
-
-  return newString;
-}
-
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  function playRound(computerChoice, humanChoice) {
+  function playRound(humanChoice, computerChoice) {
     if (computerChoice == humanChoice) {
       return `You both chose ${humanChoice}, so it's a draw! Your score is ${humanScore}, the computer's is ${computerScore}.`;
     } else if (
@@ -47,23 +22,19 @@ function playGame() {
       (humanChoice == "paper" && computerChoice == "rock")
     ) {
       ++humanScore;
-      return `You win the round! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}. Your score is ${humanScore}, the computer's is ${computerScore}.`;
+      return `You win the round! ${humanChoice} beats ${computerChoice}. Your score is ${humanScore}, the computer's is ${computerScore}.`;
     } else {
       ++computerScore;
-      return `Computer wins the round! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}. Your score is ${humanScore}, the computer's is ${computerScore}.`;
+      return `Computer wins the round! ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}, the computer's is ${computerScore}.`;
     }
   }
-  for (let i = 1; i <= 5; i++) {
-    console.log(playRound(getComputerChoice(), getHumanChoice()));
-  }
 
-  if (humanScore > computerScore) {
-    return `You win the game! Your score is ${humanScore}. The computer scored ${computerScore}.`;
-  } else if (computerScore > humanScore) {
-    return `The computer wins the game! It scored ${computerScore}. You scored ${humanScore}.`;
-  } else {
-    return "It's a draw! Play again.";
-  }
+  let buttons = document.querySelector("#buttons");
+  buttons.addEventListener("click", (e) => {
+    let targetId = e.target.id;
+    let computerChoice = getComputerChoice();
+    console.log(playRound(targetId, computerChoice));
+  });
 }
 
-console.log(playGame());
+playGame();
